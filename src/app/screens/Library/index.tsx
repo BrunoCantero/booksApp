eef9648c012612a9d9c4f7a68a383902d3371963
Jeremias Araujo <jeremias@decemberlabs.com>
2021-03-23 15:46:54 -0300

    Initial commit

---
+++ src/app/screens/Library/index.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+// import { Book } from '@interfaces/book';
+
+import BookItem from './components/BookItem';
+// import styles from './styles';
+
+function Library() {
+  // TODO: Complete
+
+  useEffect(() => {
+    // TODO: Complete
+  }, []);
+
+  return (
+    // TODO: Make a list of BookItems
+    <BookItem />
+  );
+}
+
+export default Library;
