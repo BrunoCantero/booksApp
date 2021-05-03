30a4900d160856314796b4e64a18679accfc1390
Bruno Cantero <47759015+BrunoCantero@users.noreply.github.com>
2021-05-03 00:10:52 -0300

    stacknavigator updated with bookdetails screen

--- src/app/components/AppNavigator/index.tsx
+++ src/app/components/AppNavigator/index.tsx
@@ -2,13 +2,46 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Routes from '@constants/routes';
 import Library from '@screens/Library';
+import BookDetail from '@screens/BookDetail';
+import { colors } from '@constants/colors';
+
+const { white, skyblue } = colors;
 
 const StackNavigator = createStackNavigator();
 
 function AppNavigator() {
   return (
-    <StackNavigator.Navigator>
-      <StackNavigator.Screen name={Routes.LIBRARY} component={Library} options={{ title: 'Library' }} />
+    <StackNavigator.Navigator initialRouteName="LIBRARY">
+      <StackNavigator.Screen
+        name={Routes.LIBRARY}
+        component={Library}
+        options={{
+          title: 'LIBRARY',
+          headerTintColor: white,
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontWeight: 'bold'
+          },
+          headerStyle: {
+            backgroundColor: skyblue
+          }
+        }}
+      />
+      <StackNavigator.Screen
+        name={Routes.BOOKDETAIL}
+        component={BookDetail}
+        options={{
+          title: 'LIBRARY',
+          headerTintColor: white,
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontWeight: 'bold'
+          },
+          headerStyle: {
+            backgroundColor: skyblue
+          }
+        }}
+      />
     </StackNavigator.Navigator>
   );
 }
