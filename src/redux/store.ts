334079b9629d89d67d7a2aaf929ec882f13e48ae
Bruno Cantero <47759015+BrunoCantero@users.noreply.github.com>
2021-05-03 00:14:32 -0300

    updated store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,9 +4,13 @@ import thunk from 'redux-thunk';
 import book from './books/reducer';
 
 const rootReducer = combineReducers({ book });
+export type RootState = ReturnType<typeof rootReducer>;
 
 const middlewares = [applyMiddleware(thunk)];
 
-const store = createStore(rootReducer, compose(...middlewares));
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(...middlewares));
 
 export default store;
