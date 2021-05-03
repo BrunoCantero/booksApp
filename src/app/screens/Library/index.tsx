adaacfeff891ef22dc684bf46fe7dec6e86fa08c
Bruno Cantero <47759015+BrunoCantero@users.noreply.github.com>
2021-05-03 00:17:27 -0300

    library screen finished

--- src/app/screens/Library/index.tsx
+++ src/app/screens/Library/index.tsx
@@ -1,19 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, View, FlatList } from 'react-native';
 // import { Book } from '@interfaces/book';
+import { getBooks } from '@redux/books/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@redux/store';
+import { StackNavigationProp } from '@react-navigation/stack';
 
 import BookItem from './components/BookItem';
-// import styles from './styles';
+import styles from './styles';
 
-function Library() {
+interface LibraryProps {
+  navigation: StackNavigationProp<any, any>;
+}
+
+function Library({ navigation }: LibraryProps) {
   // TODO: Complete
+  const [refreshing, setRefreshing] = useState(false);
+  const dispatch = useDispatch();
+
+  const fetchBooks = async () => dispatch(await getBooks());
+  const books = useSelector((store: RootState) => store.book.books);
+
+  const handleRefresh = async () => {
+    setRefreshing(true);
+    await fetchBooks();
+    setRefreshing(false);
+  };
 
   useEffect(() => {
-    // TODO: Complete
+    handleRefresh();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     // TODO: Make a list of BookItems
-    <BookItem />
+    <View style={styles.container}>
+      <ScrollView>
+        <FlatList
+          data={books}
+          onRefresh={handleRefresh}
+          refreshing={refreshing}
+          renderItem={({ item }) => (
+            <BookItem
+              handlePress={() => navigation.navigate('BOOKDETAIL', { item })}
+              thumb={item.imageUrl}
+              key={item.id}
+              title={item.title}
+              author={item.author}
+            />
+          )}
+          keyExtractor={item => item.id}
+        />
+      </ScrollView>
+    </View>
   );
 }
 
