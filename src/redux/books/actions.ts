45d368da7054032625657edd084b8f466beb0889
Bruno Cantero <47759015+BrunoCantero@users.noreply.github.com>
2021-05-03 00:13:45 -0300

    books actions

--- src/redux/books/actions.ts
+++ src/redux/books/actions.ts
@@ -1,13 +1,24 @@
-// import BookService from '@services/BookService';
+// import { BookState } from '@interfaces/book';
+import BookService from '@services/BookService';
 
 export const actions = {
   // TODO: Complete redux book actions
+  GET_BOOKS: 'GET_BOOKS'
 } as const;
 
-export const actionCreators = {
-  getBooks: () => {
-    // TODO: Complete getBooks action
-  }
+export const getBooks = () => {
+  // TODO: Complete getBooks action
+  return async (dispatch: any) => {
+    try {
+      const response = await BookService.getBooks();
+      if (response.ok) {
+        dispatch({
+          type: actions.GET_BOOKS,
+          payload: response.data
+        });
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
 };
-
-export default actionCreators;
