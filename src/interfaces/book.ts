fd65a74c68fda7d076d197261df443e3486b3efd
Bruno Cantero <47759015+BrunoCantero@users.noreply.github.com>
2021-05-03 00:14:19 -0300

    bookState interface

--- src/interfaces/book.ts
+++ src/interfaces/book.ts
@@ -1,3 +1,14 @@
 export interface Book {
   // TODO: Complete
 }
+
+export interface BookState {
+  book: any;
+  id: number;
+  author: string;
+  title: string;
+  genre: string;
+  publisher: string;
+  year: string;
+  imageUrl: string | null;
+}
