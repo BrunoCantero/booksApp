857d6fc425474871c6d295d4ae60956a14f4ed9b
Bruno Cantero <47759015+BrunoCantero@users.noreply.github.com>
2021-05-03 00:12:45 -0300

    getBooks service

--- src/services/BookService.ts
+++ src/services/BookService.ts
@@ -3,6 +3,14 @@ import { BOOKS_MOCK } from '@constants/mockBooks';
 export default {
   getBooks: () => {
     // TODO: Complete => Return a new promise with BOOKS_MOCK and ok: true
-    return { ok: true, data: BOOKS_MOCK };
+    return new Promise<any>((resolve, reject) => {
+      try {
+        const books = BOOKS_MOCK;
+        if (books) resolve({ ok: true, data: books });
+      } catch (error) {
+        reject(error);
+      }
+    });
+    // return { ok: true, data: BOOKS_MOCK };
   }
 };
