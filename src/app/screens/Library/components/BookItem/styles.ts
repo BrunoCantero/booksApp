52bbaa8fb84e66233bc2611d8d9b7e0ed79b59e5
Bruno Cantero <47759015+BrunoCantero@users.noreply.github.com>
2021-05-03 00:15:34 -0300

    bookitems styles

--- src/app/screens/Library/components/BookItem/styles.ts
+++ src/app/screens/Library/components/BookItem/styles.ts
@@ -1,5 +1,20 @@
 import { StyleSheet } from 'react-native';
+import { colors } from '@constants/colors';
+
+const { white } = colors;
 
 export default StyleSheet.create({
-  // TODO: Complete
+  item: {
+    backgroundColor: white,
+    width: 350,
+    margin: 10,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    flexDirection: 'row'
+  },
+  image: { margin: 15, width: 50, height: 80 },
+  bookTitle: { fontSize: 15, paddingVertical: 5, fontWeight: 'bold' },
+  bookSubtitle: { fontSize: 12 },
+  textContainer: { flex: 1, justifyContent: 'space-between' }
 });
