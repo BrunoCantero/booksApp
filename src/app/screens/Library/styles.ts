24616869b1c49179ba0354daf1be9477acf8eba7
Bruno Cantero <47759015+BrunoCantero@users.noreply.github.com>
2021-05-03 00:17:02 -0300

    library styles

--- src/app/screens/Library/styles.ts
+++ src/app/screens/Library/styles.ts
@@ -1,5 +1,16 @@
 import { StyleSheet } from 'react-native';
+import { colors } from '@constants/colors';
+
+const { skyBlueDefault } = colors;
 
 export default StyleSheet.create({
   // TODO: Complete
+  container: {
+    backgroundColor: skyBlueDefault,
+    height: '100%',
+    position: 'absolute',
+    width: '100%',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
 });
